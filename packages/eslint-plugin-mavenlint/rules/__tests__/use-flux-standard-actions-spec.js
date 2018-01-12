c407ad7bf58746e2e815905b541156871bb3ecea
Andrew Huth <ahuth@mavenlink.com>
2018-01-12 12:31:01 -0800

    Add more cases for the `use-flux-standard-actions-test`

--- packages/eslint-plugin-mavenlint/rules/__tests__/use-flux-standard-actions-spec.js
+++ packages/eslint-plugin-mavenlint/rules/__tests__/use-flux-standard-actions-spec.js
@@ -5,12 +5,29 @@ const ruleTester = new RuleTester();
 
 ruleTester.run('use-flux-standard-actions', rule, {
   valid: [
+    // Basic case with only a type property.
     {
       code: 'function test() { return { type: "FOO" }; }',
       filename: 'lib/action-creators/foo.js',
     },
+    // Non-compliant action that's not in an action-creator file.
+    {
+      code: 'function test() { return { type: "FOO", data: "BAR" }; }',
+      filename: 'lib/selectors/foo.js',
+    },
+    // Non-compliant action doesn't even have a type property.
+    {
+      code: 'function test() { return { data: "BAR" }; }',
+      filename: 'lib/action-creators/foo.js',
+    },
+    // Thunk action.
+    {
+      code: 'function test() { return function (dispatch) { dispatch({ foo: "BAR" }); } }',
+      filename: 'lib/action-creators/foo.js',
+    },
   ],
   invalid: [
+    // Basic case with a non compliant property.
     {
       code: 'function test() { return { type: "FOO", data: "BAR" } }',
       filename: 'lib/action-creators/foo.js',
