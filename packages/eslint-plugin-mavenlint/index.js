d5172022e982e4427ab3cb23352b529adb501b56
Andrew Huth <ahuth@mavenlink.com>
2018-01-17 16:21:04 -0800

    Add a rule prohibiting using string interpolation for JSX element classnames
    Instead, folks should be using CSS composition.

--- packages/eslint-plugin-mavenlint/index.js
+++ packages/eslint-plugin-mavenlint/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   rules: {
+    'use-css-composition': require('./rules/use-css-composition'),
     'use-flux-standard-actions': require('./rules/use-flux-standard-actions'),
   },
 };
