cc2607abaf52d78c9204ab65060dc657d05f4181
Andrew Huth <ahuth@mavenlink.com>
2018-03-22 13:02:39 -0700

    Simplify the failure message

--- lib/rubocop/cop/mavenlint/unsafe-mass-assignment.rb
+++ lib/rubocop/cop/mavenlint/unsafe-mass-assignment.rb
@@ -4,10 +4,7 @@ module RuboCop
   module Cop
     module Mavenlint
       class UnsafeMassAssignment < RuboCop::Cop::Cop
-        MSG  = <<~MSG
-          Please don't allow mass assignment of foreign key columns, which can result in setting a
-          relationship with an object that the user is not allowed to interact with.
-        MSG
+        MSG = "Don't allow mass-assignment of foreign key columns".freeze
 
         def on_send(node)
           return unless node.command?(:attr_accessible)
