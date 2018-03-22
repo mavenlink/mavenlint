c3c13c5d72e1b6b6ee64688b9d997b23c94adb93
Andrew Huth <ahuth@mavenlink.com>
2018-03-22 13:02:51 -0700

    Document the UnsafeMassAssignment cop

--- lib/rubocop/cop/mavenlint/unsafe-mass-assignment.rb
+++ lib/rubocop/cop/mavenlint/unsafe-mass-assignment.rb
@@ -3,6 +3,17 @@ require 'rubocop'
 module RuboCop
   module Cop
     module Mavenlint
+      # Identify usages of mass assignment with potentially 'unsafe' columns allowed.
+      #
+      # For example
+      #
+      #   class SomeModel
+      #     attr_accessible :account_id
+      #   end
+      #
+      # Allowing mass assignment of a foreign key column is dangerous for models that are created
+      # or updated through a publicly accessible endpoint, because the associated model isn't
+      # necessarily loaded and ran through security checks.
       class UnsafeMassAssignment < RuboCop::Cop::Cop
         MSG = "Don't allow mass-assignment of foreign key columns".freeze
 
