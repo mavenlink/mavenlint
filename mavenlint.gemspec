c63436900cf8107e55ef50fe8567e77fdb6168b2
Andrew Huth <ahuth@mavenlink.com>
2018-03-22 13:26:26 -0700

    Test our first Rubocop matcher

--- mavenlint.gemspec
+++ mavenlint.gemspec
@@ -6,4 +6,7 @@ Gem::Specification.new do |s|
   s.authors     = ["Mavenlnk"]
   s.files       = ["rubocop.yml"]
   s.homepage    = "https://github.com/mavenlink/mavenlint"
+
+  s.add_development_dependency "rspec", "3.7.0"
+  s.add_development_dependency "rubocop", "~> 0.49"
 end
---
+++ spec/rubocop/cop/mavenlint/unsafe_mass_assignment_spec.rb
@@ -0,0 +1,19 @@
+require 'rubocop/cop/mavenlint/unsafe-mass-assignment'
+
+describe RuboCop::Cop::Mavenlint::UnsafeMassAssignment do
+  let(:config) { RuboCop::Config.new }
+  subject(:cop) { described_class.new(config) }
+
+  it 'registers an offense allowing mass assignment of foreign keys' do
+    expect_offense(<<~RUBY)
+      attr_accessible :story, :name, :account_id, :level
+      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Don't allow mass-assignment of foreign key columns
+    RUBY
+  end
+
+  it 'does not register an offense when not allowing mass assignment of foreign keys' do
+    expect_no_offenses(<<~RUBY)
+      attr_accessible :story, :name, :account, :level
+    RUBY
+  end
+end
