ad2c764ece28295d57870110408267d6a0100f1b
Andrew Huth <ahuth@mavenlink.com>
2018-04-13 15:55:33 -0700

    Organize the organization

--- spec/rubocop/cop/mavenlint/mocking_framework_spec.rb
+++ spec/rubocop/cop/mavenlint/mocking_framework_spec.rb
@@ -6,24 +6,26 @@ RSpec.describe RuboCop::Cop::Mavenlint::MockingFramework do
   subject(:cop) { described_class.new(config) }
 
   describe 'stubs' do
-    it 'detects simple stubs' do
-      expect_offense(<<~RUBY)
-        stub(instance).should_email? { true }
-        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use rspec-mocks
-      RUBY
-    end
+    describe 'simple examples' do
+      it 'detects simple stubs' do
+        expect_offense(<<~RUBY)
+          stub(instance).should_email? { true }
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use rspec-mocks
+        RUBY
+      end
 
-    it 'detects simple stubs' do
-      expect_offense(<<~RUBY)
-        stub(user).is_administrator? { false }
-        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use rspec-mocks
-      RUBY
-    end
+      it 'detects simple stubs' do
+        expect_offense(<<~RUBY)
+          stub(user).is_administrator? { false }
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Use rspec-mocks
+        RUBY
+      end
 
-    it 'passes when rspec-mocks is used' do
-      expect_no_offenses(<<~RUBY)
-        expect(instance).to receve(:should_email?) { true }
-      RUBY
+      it 'passes when rspec-mocks is used' do
+        expect_no_offenses(<<~RUBY)
+          expect(instance).to receve(:should_email?) { true }
+        RUBY
+      end
     end
   end
 end
