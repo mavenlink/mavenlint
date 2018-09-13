4cd491480f44e772f6b6f2ba02feb469d569ad77
Andrew Huth <ahuth@mavenlink.com>
2018-09-13 11:32:34 -0700

    Doh, spelling

--- lib/mavenlint.rb
+++ lib/mavenlint.rb
@@ -1,3 +1,3 @@
 require 'rubocop/cop/mavenlint/unsafe_mass_assignment'
 require 'rubocop/cop/mavenlint/use_application_record'
-require 'rubocop/cop/mavenlint/use_safe_capybara_matchers'
+require 'rubocop/cop/mavenlint/use_fast_capybara_matchers'
