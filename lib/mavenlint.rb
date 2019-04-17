9398e5144e95eba9f1fcef603828b8f4baa6602f
Brent Gaither <brent.w.gaither@gmail.com>
2019-04-17 08:44:25 -0600

    Add a linter to ensure controllers inherit from appllication controller

--- lib/mavenlint.rb
+++ lib/mavenlint.rb
@@ -1,3 +1,4 @@
 require 'rubocop/cop/mavenlint/unsafe_mass_assignment'
 require 'rubocop/cop/mavenlint/use_application_record'
+require 'rubocop/cop/mavenlint/use_application_controller'
 require 'rubocop/cop/mavenlint/use_fast_capybara_matchers'
