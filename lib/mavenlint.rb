4dee802fefb7ee7513e2bd17e3e774de22f476fb
Jared Nielson <jnielson@mavenlink.com>
2023-06-07 14:25:25 -0600

    Make requiring no longer necessary

--- lib/mavenlint.rb
+++ lib/mavenlint.rb
@@ -1,11 +1,3 @@
 # frozen_string_literal: true
 
-require 'rubocop/cop/mavenlint/unsafe_mass_assignment'
-require 'rubocop/cop/mavenlint/use_application_record'
-require 'rubocop/cop/mavenlint/use_application_controller'
-require 'rubocop/cop/mavenlint/use_api_controller'
-require 'rubocop/cop/mavenlint/use_fast_capybara_matchers'
-require 'rubocop/cop/mavenlint/direct_factory_bot_invocation'
-require 'rubocop/cop/mavenlint/require_slow_helpers'
-require 'rubocop/cop/mavenlint/bigint_for_migration_keys'
-require 'rubocop/cop/mavenlint/no_dependent_destroy_account'
+Dir[File.join(__dir__, 'rubocop/cop/mavenlint', '*.rb')].each { |file| require file }
