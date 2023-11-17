7be40e69a0fb9ae7721918b7f2cb39ad2e03ba5d
Ben Nappier <ben@mavenlink.com>
2023-11-17 08:03:26 -0700

    remove dot-notation enforcement

--- packages/eslint-config-mavenlint/index.js
+++ packages/eslint-config-mavenlint/index.js
@@ -17,6 +17,7 @@ module.exports = {
     'arrow-body-style': 'off',
     'arrow-parens': 'off',
     'func-names': 'off',
+    'dot-notation': 'off',
     'no-console': 'error',
     'no-multiple-empty-lines': ["error", { "max": 1 }],
     'no-use-before-define': ["error", { "functions": false, "classes": true }],
